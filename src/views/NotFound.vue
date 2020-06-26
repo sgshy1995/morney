febeda4f1cbf82c2b8cf254e0c903f319739040c
EdenSheng <singlesaul@163.com>
2020-06-26 15:36:05 +0800

    添加404

---
+++ src/views/NotFound.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <div>访问页面不存在，请检查路径</div>
+    <router-link to="/">返回首页</router-link>
+  </div>
+</template>
+
+<script lang="ts">
+  import Vue from 'vue';
+  import {Component} from 'vue-property-decorator';
+
+  @Component
+  export default class NotFound extends Vue {
+
+  }
+</script>
+
+<style scoped lang="scss">
+
+</style>
