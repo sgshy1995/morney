72224340ff7cf7f0c871affe7744f4d8d18d61e4
EdenSheng <singlesaul@163.com>
2020-06-27 15:38:01 +0800

    添加 导航栏 样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -2,13 +2,22 @@
   <div class="app">
     <el-row type="flex" justify="center">
       <el-col :span="8">
-        <router-link to="/money">首页</router-link>
+        <router-link to="/money" class="wrapper">
+          <i class="el-icon-mobile icon"></i>
+          <span>记账</span>
+        </router-link>
       </el-col>
       <el-col :span="8">
-        <router-link to="/labels">标签</router-link>
+        <router-link to="/labels" class="wrapper">
+          <i class="el-icon-collection-tag icon"></i>
+          <span>标签</span>
+        </router-link>
       </el-col>
       <el-col :span="8">
-        <router-link to="statistics">统计</router-link>
+        <router-link to="statistics" class="wrapper">
+          <i class="el-icon-notebook-2 icon"></i>
+          <span>统计</span>
+        </router-link>
       </el-col>
     </el-row>
   </div>
@@ -35,9 +44,31 @@
 
   .el-col {
     display: flex;
+    flex-direction: column;
     justify-content: center;
     align-items: center;
     height: 100%;
+  }
+  .wrapper{
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+    text-decoration: none;
+    color: inherit;
+    font-size: 14px;
+    border: 1px solid black;
+    padding: 5px 20px;
+    border-radius: 5px;
+  }
+  .wrapper:focus{
+    color: red;
+    border-color: red;
+  }
+
+  .icon{
+    width: 1em;
+    height: 1em;
   }
 
 </style>
